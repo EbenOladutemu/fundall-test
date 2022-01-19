3eaefbdec6b01e1ad24d77aa8f1858e45aa70572
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2022-01-19 14:15:39 +0100

    Start projects. Add images, font and styles

---
+++ src/components/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="">
+      <img src="./../assets/images/fundall.svg" alt="">
+      <slot name="left"></slot>
+    </div>
+    <div>
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
